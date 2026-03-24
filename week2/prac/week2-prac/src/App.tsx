0d64392894c65a948473ca5078040aaeb8b72f28
aram426 <amy0426@cau.ac.kr>
2026-03-24 19:36:25 +0900

    2주차 브랜치 생성

--- week2/prac/week2-prac/src/App.tsx
+++ week2/prac/week2-prac/src/App.tsx
@@ -1,24 +1,15 @@
-import './App.css'
-import {useState} from 'react'
+import ButtonGroup from './components/ButtonGroup';
+import { useCount } from './context/CounterProvider';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const handleIncreaseNumber = () => {
-    setCount(prev => prev + 1);
-  }
-
-  const handleDecreaseNumber = () => {
-    setCount(prev => prev - 1);
-  }
+  const { count } = useCount();
 
   return (
     <>
       <h1>{count}</h1>
-      <button onClick={handleIncreaseNumber}>숫자 증가</button>
-      <button onClick={handleDecreaseNumber}>숫자 감소</button>
+      <ButtonGroup />
     </>
-  )
+  );
 }
 
 export default App;
