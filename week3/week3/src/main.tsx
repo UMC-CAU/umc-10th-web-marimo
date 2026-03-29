1e5e36bf2e5fce6be0d1fbafe105434ed675c53f
aram426 <amy0426@cau.ac.kr>
2026-03-30 01:05:07 +0900

    SPA 만들어보기

--- week3/week3/src/main.tsx
+++ week3/week3/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { PathProvider } from './PathContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PathProvider>
+      <App />
+    </PathProvider>
   </StrictMode>,
 )
