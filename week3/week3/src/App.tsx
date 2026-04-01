2d65bc8070ce09f4917207a5abb1dc234ecec5e6
aram426 <amy0426@cau.ac.kr>
2026-04-01 20:47:59 +0900

    영화 API연결 실습 끝

--- week3/week3/src/App.tsx
+++ week3/week3/src/App.tsx
@@ -1,21 +1,36 @@
-import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+import HomePage from './pages/home';
+import Movies from './pages/movies';
+import NotFound from './pages/not-found';
+import RootLayout from './layout/root-layout';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <h1>홈 페이지입니다.</h1>,
-    // 매칭 실패/라우트 에러 시 보여줄 UI
-    errorElement: <h1>너는 없는 경로에 들어왔다 ^ㅁ^ 야호~!</h1>,
-  },
-  {
-    path: '/movies',
-    element: <h1>영화 페이지 입니다.</h1>,
+    element: <RootLayout />,
+    errorElement: <NotFound />,
+
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'movies',
+        element: <Movies />
+      },
+      {
+        path: 'movies/:movieId',
+        element: <Movies />
+      },
+    ],
   },
 ]);
 
 function App() {
   return <RouterProvider router={router} />;
-}
+};
 
 export default App;
---
+++ week3/week3/src/pages/movies.tsx
@@ -0,0 +1,40 @@
+import {useParams} from 'react-router-dom';
+import {useState, useEffect} from 'react';
+import {type Movie, type MovieResponse} from '../types/movie';
+import axios from 'axios';
+
+const MoviesPage = () => {
+  const params = useParams();
+  const [movies, setMovies] =useState<Movie[]>([]);
+
+  console.log(movies);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const {data} = await axios.get<MovieResponse>(
+        `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`,
+        {
+          headers: {
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYWM5MzI3YjFlODg3ZmJmZDRkOWZkOTdkNWYwNDhmNCIsIm5iZiI6MTc3NDg2Mzk0NC44NzgsInN1YiI6IjY5Y2E0NjQ4N2ZlNWE0NjI5NzFlY2U4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lUy95AcWLdz_E8WOV6LPrmR9IwqsiZlClH12wZwLzX8',
+          },
+        }
+      );
+      setMovies(data.results);
+    };
+
+    fetchMovies();
+  }, []);
+  
+  return (
+    <ul>
+      {movies?.map((movie) => (
+        <li key={movie.id} style={{ listStyle: 'none' }}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default MoviesPage;
+
