c230f7b5210b1fadecce6832fc4992d0263772f9
aram426 <amy0426@cau.ac.kr>
2026-06-05 17:11:58 +0900

    CHORE: add vercel.json and routing

--- week6/src/App.tsx
+++ week6/src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import UseCallbackPage from './07-useCallback-Memo/useCallbackPage'
 
+function MovieDetailPage() {
+  return <div>영화 상세 페이지</div>
+}
+
 function App() {
-  return <UseCallbackPage />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UseCallbackPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
